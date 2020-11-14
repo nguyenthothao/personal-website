ca8b1fa9a087815a283fa91f3e9fdde86d2e7eeb
Thao <nguyenthothao1996@gmail.com>
2020-11-14 21:18:55 +0700

    Navbar and header

--- components/nav.js
+++ components/nav.js
@@ -1,29 +1,75 @@
-import Link from 'next/link'
+import Link from "next/link";
+import HomeIcon from "@material-ui/icons/Home";
+import styles from "./Nav.module.css";
 
-const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
-]
+const language = [
+  { href: "https://github.com/vercel/next.js", label: "Language" },
+  { href: "https://github.com/vercel/next.js", label: "English" },
+  { href: "https://github.com/vercel/next.js", label: "VietNam" },
+  { href: "https://github.com/vercel/next.js", label: "Spanish" },
+];
+const homePage = [
+  { href: "/", label: "Home" },
+  { href: "https://nextjs.org/docs", label: "Blog" },
+  { href: "https://nextjs.org/docs", label: "Docs" },
+];
 
 export default function Nav() {
   return (
     <nav>
-      <ul className="flex justify-between items-center p-8">
-        <li>
-          <Link href="/">
-            <a className="text-blue-500 no-underline">Home</a>
-          </Link>
-        </li>
+      <div className="navbar flex justify-between items-center p-2 bg-blue-600 ">
         <ul className="flex justify-between items-center space-x-4">
-          {links.map(({ href, label }) => (
+          {homePage.map(({ href, label }) => (
             <li key={`${href}${label}`}>
-              <a href={href} className="btn-blue no-underline">
-                {label}
-              </a>
+              <Link href={href}>
+                <a className="text-white no-underline uppercase font-bold text-xs">
+                  {label}
+                </a>
+              </Link>
             </li>
           ))}
         </ul>
-      </ul>
+        <ul className="flex justify-between items-center space-x-4">
+          {language.map(({ href, label }) => (
+            <li key={`${href}${label}`}>
+              <Link href={href}>
+                <a className="text-white uppercase font-bold">{label}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="header items-center bg-gray-500 py-6">
+        <div className="flex justify-center uppercase font-bold text-white text-center list-none px-8 space-x-24">
+          <li className="">
+            <Link href="/">
+              <a>
+                <HomeIcon />
+              </a>
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/">
+              <a>about</a>
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/">
+              <a>skill</a>
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/">
+              <a>work</a>
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/">
+              <a>experience</a>
+            </Link>
+          </li>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
