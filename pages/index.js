887595732eb490a739549d2f552af63f07c5652f
Thao <nguyenthothao1996@gmail.com>
2020-11-14 21:29:00 +0700

    Add component-level css

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,8 @@ export default function IndexPage() {
   return (
     <div>
       <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Thao website with Nextjs and Tailwindcss
-          Personal website
-        </h1>
+      <div className="box-border h-20 w-32 p-4 border-4 border-gray-400 bg-gray-200">
+
       </div>
     </div>
   )
